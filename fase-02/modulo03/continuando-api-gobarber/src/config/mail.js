8a8d03222e9b55971fc34e0defc7942ce7907b81
grios <60109015+grioos@users.noreply.github.com>
2020-09-22 17:01:21 -0300

    environment variables

--- fase-02/modulo03/continuando-api-gobarber/src/config/mail.js
+++ fase-02/modulo03/continuando-api-gobarber/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    host: process.env.MAIL_HOST,
+    port: process.port.MAIL_PORT,
     secure: false,
     auth: {
-        user: '0e9c70cade099b',
-        pass: 'a77851eaa4afc5',
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
     },
     default: {
         from: 'Equipe GoBarber <noreply@gobarber.com>',
--- fase-02/modulo03/continuando-api-gobarber/src/config/sentry.js
+++ fase-02/modulo03/continuando-api-gobarber/src/config/sentry.js
@@ -1,4 +1,3 @@
 export default {
-    dsn:
-        'https://248db59210be42aa952495ccc715cbb4@o451546.ingest.sentry.io/5437612',
+    dsn: process.env.SENTR_DSN,
 };
