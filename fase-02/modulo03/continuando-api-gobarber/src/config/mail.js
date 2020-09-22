74f1fc5e034e2facb09faffff91f379b29fb5c14
grios <60109015+grioos@users.noreply.github.com>
2020-09-22 10:34:17 -0300

    nodemailer config

---
+++ fase-02/modulo03/continuando-api-gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '0e9c70cade099b',
+        pass: 'a77851eaa4afc5',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
