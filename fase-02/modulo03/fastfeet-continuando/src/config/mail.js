cbcf710058ebd603fc965ae09c37861b353f9823
grios <60109015+grioos@users.noreply.github.com>
2020-09-27 09:05:50 -0300

    mail service

---
+++ fase-02/modulo03/fastfeet-continuando/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '9968bc7c5c425b',
+        pass: '43c8a6f33ae3e9',
+    },
+    default: {
+        from: 'Equipe FastFeet <norepley@fastfeet.com>',
+    },
+};
