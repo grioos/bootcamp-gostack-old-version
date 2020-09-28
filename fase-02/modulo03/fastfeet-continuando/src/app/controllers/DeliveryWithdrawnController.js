4588204ef870f93a742cbbabeb88909769ffa6bb
grios <60109015+grioos@users.noreply.github.com>
2020-09-28 11:02:30 -0300

    delivery withdrawn find all with count

--- fase-02/modulo03/fastfeet-continuando/src/app/controllers/DeliveryWithdrawnController.js
+++ fase-02/modulo03/fastfeet-continuando/src/app/controllers/DeliveryWithdrawnController.js
@@ -1,16 +1,42 @@
-import { startOfHour, setHours, isWithinInterval } from 'date-fns';
+import {
+    startOfHour,
+    setHours,
+    isWithinInterval,
+    startOfDay,
+    endOfDay,
+} from 'date-fns';
+import { Op } from 'sequelize';
 
 import Delivery from '../models/Delivery';
 
 class DeliveryWithdrawnController {
     async update(req, res) {
-        const delivery = await Delivery.findByPk(req.params.id);
+        const delivery = await Delivery.findByPk(req.params.id, {
+            where: {
+                canceled_at: null,
+                end_date: null,
+            },
+        });
 
         if (!delivery) {
             return res.status(400).json({ error: 'Delivery does not exists' });
         }
 
         const currentDate = new Date();
+
+        const deliverymanCount = await Delivery.findAndCountAll({
+            where: {
+                deliveryman_id: delivery.deliveryman_id,
+                start_date: {
+                    [Op.between]: [
+                        startOfDay(currentDate),
+                        endOfDay(currentDate),
+                    ],
+                },
+            },
+        });
+
+        console.log(deliverymanCount);
 
         if (
             !isWithinInterval(currentDate, {
@@ -22,6 +48,13 @@ class DeliveryWithdrawnController {
                 error:
                     'Deliveries can be only withdrawn between 8:00h - 18:00h',
             });
+
+        if (deliverymanCount.count > 5) {
+            return res.status(401).json({
+                error:
+                    "You've reached your max number of withdrawns in one day",
+            });
+        }
 
         await delivery.update({
             start_date: currentDate,
