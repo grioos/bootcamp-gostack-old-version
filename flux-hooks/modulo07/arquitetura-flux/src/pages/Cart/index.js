cfcda1d27ba8d9979ace9b0884bcf48d2d45a6f5
unknown <gabriel.al.rio@gmail.com>
2020-10-09 13:08:53 -0300

    setting routes

---
+++ flux-hooks/modulo07/arquitetura-flux/src/pages/Cart/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+// import { Container } from './styles';
+
+function Cart() {
+  return <h1>Carrinho</h1>;
+}
+
+export default Cart;
