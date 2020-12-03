cc4688fd04d7f44c2be921b25ece807ad00a1766
grios <gabriel.al.rio@gmail.com>
2020-12-03 12:12:01 -0300

    it should be able to register users

--- testes-deploy-nodejs/modulo011/server/__tests__/integration/user.test.js
+++ testes-deploy-nodejs/modulo011/server/__tests__/integration/user.test.js
@@ -1,16 +1,16 @@
-import request from 'supertest'
-import app from '../../src/app'
+import request from 'supertest';
+import app from '../../src/app';
 
 describe('User', () => {
-  it('Should be able to register' async () => {
+  it('Should be able to register', async () => {
     const response = await request(app)
       .post('/users')
       .send({
         name: 'Diego Fernandes',
         email: 'diego@rocketseat.com.br',
-        password_hash: '123456'
-      })
+        password_hash: '123456',
+      });
 
-    expect(response.body).toHaveProperty('id')
-  })
-})
+    expect(response.body).toHaveProperty('id');
+  });
+});
