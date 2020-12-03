ba5c9520f8062a271a4be6b7534c8d1cfa3769e5
grios <gabriel.al.rio@gmail.com>
2020-12-03 11:56:51 -0300

    add user models

---
+++ testes-deploy-nodejs/modulo011/server/src/app/models/User.js
@@ -0,0 +1,19 @@
+import sequelize from 'sequelize';
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default User;
