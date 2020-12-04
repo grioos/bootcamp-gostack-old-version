c37f5549ea09b7e4c5941ebf8d49548069963a1f
grios <gabriel.al.rio@gmail.com>
2020-12-04 12:01:52 -0300

    password hash

--- testes-deploy-nodejs/modulo011/server/__tests__/integration/user.test.js
+++ testes-deploy-nodejs/modulo011/server/__tests__/integration/user.test.js
@@ -1,17 +1,30 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 import truncate from '../util/truncate';
+import User from '../../src/app/models/User';
 
 describe('User', () => {
   beforeEach(async () => {
     await truncate();
   });
 
+  it('Should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'Diego Fernandes',
+      email: 'diego@rocketseat.com.br',
+      password: '123456',
+    });
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+
   it('Should be able to register', async () => {
     const response = await request(app).post('/users').send({
       name: 'Diego Fernandes',
       email: 'diego@rocketseat.com.br',
-      password_hash: '123456',
+      password: '123456',
     });
 
     expect(response.body).toHaveProperty('id');
@@ -21,13 +34,13 @@ describe('User', () => {
     await request(app).post('/users').send({
       name: 'Diego Fernandes',
       email: 'diego@rocketseat.com.br',
-      password_hash: '123456',
+      password: '123456',
     });
 
     const response = await request(app).post('/users').send({
       name: 'Diego Fernandes',
       email: 'diego@rocketseat.com.br',
-      password_hash: '123456',
+      password: '123456',
     });
 
     expect(response.status).toBe(400);
