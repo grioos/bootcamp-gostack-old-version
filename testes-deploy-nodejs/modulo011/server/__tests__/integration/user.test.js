02e7a18fb9c170fec893a18a5862583326bd6395
grios <gabriel.al.rio@gmail.com>
2020-12-04 11:19:11 -0300

    test duplicate email

--- testes-deploy-nodejs/modulo011/server/__tests__/integration/user.test.js
+++ testes-deploy-nodejs/modulo011/server/__tests__/integration/user.test.js
@@ -1,7 +1,12 @@
 import request from 'supertest';
 import app from '../../src/app';
+import truncate from '../util/truncate';
 
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('Should be able to register', async () => {
     const response = await request(app).post('/users').send({
       name: 'Diego Fernandes',
@@ -10,5 +15,21 @@ describe('User', () => {
     });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('Should not be able to register with duplicated email', async () => {
+    await request(app).post('/users').send({
+      name: 'Diego Fernandes',
+      email: 'diego@rocketseat.com.br',
+      password_hash: '123456',
+    });
+
+    const response = await request(app).post('/users').send({
+      name: 'Diego Fernandes',
+      email: 'diego@rocketseat.com.br',
+      password_hash: '123456',
+    });
+
+    expect(response.status).toBe(400);
   });
 });
