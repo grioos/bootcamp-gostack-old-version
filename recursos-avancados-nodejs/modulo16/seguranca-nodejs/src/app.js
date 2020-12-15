12d6dee8bea8936f92cdc1e42e1e7516784270c3
Gabriel Rios <gabriel.al.rio@gmail.com>
2020-12-15 11:43:44 -0300

    utilizing rate limit

--- recursos-avancados-nodejs/modulo16/seguranca-nodejs/src/app.js
+++ recursos-avancados-nodejs/modulo16/seguranca-nodejs/src/app.js
@@ -2,6 +2,9 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -29,6 +32,21 @@ class App {
             '/files',
             express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
         );
+
+        if (process.env.NODE_ENV !== 'development') {
+            this.server.use(
+                new RateLimit({
+                    store: new RateLimitRedis({
+                        client: redis.createClient({
+                            host: process.env.REDIS_HOST,
+                            port: process.env.REDIS_PORT,
+                        }),
+                    }),
+                    windowMs: 1000 * 60 * 15,
+                    max: 10,
+                })
+            );
+        }
     }
 
     routes() {
